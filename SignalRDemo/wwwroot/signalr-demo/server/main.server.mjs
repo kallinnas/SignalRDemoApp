5617f978aefeac09d8cd7a80fc5bb89790e754d4
kallinnas <kallinnas@gmail.com>
2024-10-14 10:00:12 +0300

    BE: + dist ui

---
+++ SignalRDemo/wwwroot/signalr-demo/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IUA3Q57V.mjs";import"./chunk-YV73ICUO.mjs";import"./chunk-5XGRGLDC.mjs";import"./chunk-EE6QAVAC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
