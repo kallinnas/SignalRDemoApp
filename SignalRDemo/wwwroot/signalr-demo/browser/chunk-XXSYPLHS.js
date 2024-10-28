fc60da85b530f29885a6e86513070370ae83c14e
kallinnas <kallinnas@gmail.com>
2024-10-28 21:39:06 +0200

    deploy: fix docker

--- SignalRDemo/wwwroot/signalr-demo/browser/chunk-XXSYPLHS.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./chunk-RNMDN42R.js";var p=e((s,t)=>{t.exports=XMLHttpRequest});export default p();
--- SignalRDemo/wwwroot/signalr-demo/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-IUA3Q57V.mjs";import"./chunk-YV73ICUO.mjs";import"./chunk-5XGRGLDC.mjs";import"./chunk-EE6QAVAC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
